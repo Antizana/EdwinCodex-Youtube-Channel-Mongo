781d8641d3dd1c8405400c10c2983a10e8fd4426
Antizana <edwinvillacis@yahoo.com>
2022-03-07 16:03:21 -0500

    Basics for my YouTube channel, mongo section

--- routes/api.js
+++ routes/api.js
@@ -66,4 +66,77 @@ router.get("/profile/query", (req, res) => {
     });
 });
 
+router.get("/profileById/:id", (req, res) => {
+  Profile.findById(req.params.id)
+    .then((profile) => {
+      res.json({
+        confirmation: "success",
+        data: profile,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "fail",
+        message: err.message,
+      });
+    });
+});
+
+// parse json data
+router.use(express.json());
+
+router.post("/profile", (req, res) => {
+  Profile.create(req.body)
+    .then((profile) => {
+      res.json({
+        confirmation: "success",
+        data: profile,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "fail",
+        message: err.message,
+      });
+    });
+});
+
+router.put("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  body = req.body;
+  delete body["id"];
+
+  Profile.findByIdAndUpdate(id, body, { new: true })
+    .then((profile) => {
+      res.json({
+        confirmation: "success",
+        data: profile,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "fail",
+        message: err.message,
+      });
+    });
+});
+
+router.delete("/profile/:id", (req, res) => {
+  const { id } = req.params;
+
+  Profile.findByIdAndRemove(id)
+    .then(() => {
+      res.json({
+        confirmation: "success",
+        data: "Profile " + id + " has been removed.",
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "fail",
+        message: err.message,
+      });
+    });
+});
+
 module.exports = router;
